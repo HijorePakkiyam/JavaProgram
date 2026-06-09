1447d709ee44e5705ecca0943f2d1fd1f4134b17
Hijore-P <hijore.p@outlook.com>
2026-06-10 01:48:13 +0530

    Added Java interview programs

--- JavaProgram.java
+++ JavaProgram.java
@@ -1,7 +1,33 @@
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class JavaProgram{
 
     public static void main(String[] args){
-                System.out.println("Hello world");
+
+        //find the duplicate elements
+        int[] arr = {1, 2, 3, 2, 4, 1, 5};
+        Set<Integer> unique = new HashSet<>();
+        Set<Integer> duplicates = Arrays.stream(arr).boxed().filter(e-> ! unique.add(e)).collect(Collectors.toSet());
+//        System.out.println(duplicates);
+
+        // find the first non-repeated character
+        String s = "swiss";
+
+       Character ch =  s.chars().mapToObj(c -> (char) c)
+                 .collect(Collectors.groupingBy(Function.identity(),LinkedHashMap::new,Collectors.counting()))
+                 .entrySet().stream()
+                 .filter(k -> k.getValue() == 1).map(Map.Entry::getKey)
+                 .findFirst()
+                 .orElse(null);
+//        System.out.println(ch);
+
+
+        String str = "swiss";
+
+
+
+
     }
 }
